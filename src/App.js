fb7910f0cb8aadf6f92b683eb596b7f9c25dcd6d
Fang Yao <fang7797@esri.com>
2021-12-23 10:33:05 -0800

    adding files for switch-test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import '@esri/calcite-components/dist/calcite/calcite.css';
+import { CalciteSwitch, CalciteButton } from '@esri/calcite-components-react';
+import {useState} from "react";
+
+
 
 function App() {
+  const [test, setTest] = useState(true);
+
+  const onTestButtonClick = () =>{
+    setTest(!test);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CalciteSwitch checked={true}></CalciteSwitch>
+      <CalciteButton onClick={onTestButtonClick}>Test</CalciteButton>
+
     </div>
+
+
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { applyPolyfills, defineCustomElements } from '@esri/calcite-components/dist/loader';
+
+// Apply polyfills and then define the custom elements
+// polyfills are not needed if you don't support IE11 or Edge
+applyPolyfills().then(() => {
+  defineCustomElements(window);
+});
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
